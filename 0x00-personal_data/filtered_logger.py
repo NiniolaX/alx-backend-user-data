219b4d8e043a0057e395df126716be9c6355a4be
NiniolaX <loveafinni16@gmail.com>
2024-11-08 17:18:01 +0100

    Added filter_datum metod

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env/python3
+from typing import List
 import logging
+import re
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+def filter_datum(fields: List, redaction: str, message: str, separator: str):
+    """  """
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
 
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+# class RedactingFormatter(logging.Formatter):
+#     """ Redacting Formatter class
+#         """
 
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+#     REDACTION = "***"
+#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+#     SEPARATOR = ";"
+
+#     def __init__(self):
+#         super(RedactingFormatter, self).__init__(self.FORMAT)
+
+#     def format(self, record: logging.LogRecord) -> str:
+#         NotImplementedError
