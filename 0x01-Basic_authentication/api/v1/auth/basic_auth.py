d375f966dad611e5d99a838fdf5158f40e9c0f7d
NiniolaX <loveafinni16@gmail.com>
2024-11-13 00:23:32 +0100

    Implemented Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,32 @@
 """
 Handles Basic Authentication
 """
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ Handles Basic Authentication """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Returns the Base64 part of the Authorization header
+
+        Args:
+            authorization_header (str): Authorization header
+
+        Returns:
+            (str): Base64 section of Authorization header
+        """
+        if not authorization_header:
+            return
+
+        if not isinstance(authorization_header, str):
+            return
+
+        try:
+            auth_name, base64_part = authorization_header.split()
+            if auth_name != 'Basic':
+                return
+
+            return base64_part
+        except ValueError:  # String doesn't have space after auth_name
+            return
