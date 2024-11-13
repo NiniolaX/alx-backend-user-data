0dac60954b321543634b214eb0c89772f2a6a499
NiniolaX <loveafinni16@gmail.com>
2024-11-13 08:25:48 +0100

    Added function that decodes a base64 encoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Handles Basic Authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -30,4 +31,26 @@ class BasicAuth(Auth):
 
             return base64_part
         except ValueError:  # String doesn't have space after auth_name
+            return
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ Returns the decoded value of a Base64 string
+
+        Args:
+            base64_authorization_header(str): Base64 auth header string to be
+                decoded.
+        """
+        if not base64_authorization_header:
+            return
+
+        if not isinstance(base64_authorization_header, str):
+            return
+
+        try:
+            decoded_auth_header = base64.b64decode(base64_authorization_header,
+                                                   validate=True)
+            return decoded_auth_header.decode('utf-8')
+        except Exception:  # If invalid base64 encoded bytes
             return
