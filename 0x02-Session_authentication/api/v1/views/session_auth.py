b0c3ac52f6a7eab9f294b5326a1b2b387854ccc3
NiniolaX <loveafinni16@gmail.com>
2024-11-15 22:38:28 +0100

    Fixed pycodestyle errors

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -11,25 +11,26 @@ from os import getenv
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """ Logs in a user """
-    # Extract user email and password
+    # Extract user email
     email = request.form.get('email')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
 
+    # Extract user password
     password = request.form.get('password')
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
 
     # Retrieve and validate user based on provided email and password
     users = User.search({"email": email})
     if not users:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
 
     user = users[0]  # Assumed emails were unique and extracted first match
 
     # Validate user password
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
 
     # Create Session ID for user
     from api.v1.app import auth
