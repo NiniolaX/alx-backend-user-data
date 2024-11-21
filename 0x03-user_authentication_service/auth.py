feacec723dd0f8c5d699763ab759e9100c2eb282
NiniolaX <loveafinni16@gmail.com>
2024-11-21 09:00:42 +0100

    FUpdated update_password method in Auth to delete reset_token after password reset

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -248,7 +248,8 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
             return
 
         except Exception:  # If no user is found or update_user fails
